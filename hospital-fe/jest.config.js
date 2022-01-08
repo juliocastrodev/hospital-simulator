aaa8b5006dd7479d034e18ce95e80c76644a1aa7
Julio César Castro López <julio.castro.dev@gmail.com>
2022-01-09 00:16:56 +0100

    feat: create frontend application

---
+++ hospital-fe/jest.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+  },
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+}
